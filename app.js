92918fd62fd6b241a098bbb89c600883768e73e5
Kai Yan <u3514123@connect.hku.hk>
2020-11-18 21:29:29 -0500

    add socket.io

--- app.js
+++ app.js
@@ -3,22 +3,29 @@
 const express = require('express')
     , path = require('path')
 //   , logger = require('morgan')
-    , session = require("express-session")
+    , expressSession = require("express-session")
     , cookieParser = require('cookie-parser')
     , bodyParser = require('body-parser')
     , ioCookie = require('socket.io-cookie')
+    , sharedSession = require('express-socket.io-session')
     , login_router = require('./controllers/login-router')
     , chatroom_router = require('./controllers/chatroom-router')
-//   , login  = require('./controllers/router_login')
-//   , admin  = require('./controllers/router_admin')
-//   , play   = require('./controllers/router_play')
+    , Operator = require('./models/operator')
     , config = require('./config')
   ;
 
-const app = express()
-  , server = require('http').createServer(app)
-//   , io = require('./controllers/socket').listen(server)
-  ;
+const app = express();
+const server = require('http').createServer(app);
+
+const session = expressSession({
+    secret: config.secret,
+    resave: true,
+    saveUninitialized: true,
+});
+
+const operator = new Operator(server);
+operator.use(ioCookie);
+operator.use(sharedSession(session, cookieParser(config.secret)));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -27,11 +34,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cookieParser(config.secret));
-app.use(session({
-    secret: config.secret,
-    resave: true,
-    saveUninitialized: true,
-}))
+app.use(session);
 app.use(express.static(path.join(__dirname, config.staticPath)));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
@@ -42,12 +45,6 @@ if (config.staticPath == 'test') {
     });
 }
 
-
-// app.use(login.get);
-// app.use(login.post);
-// app.use(admin.get);
-// app.use(admin.post);
-// app.use(play.get);
 app.use(login_router);
 app.use(chatroom_router);
 
@@ -57,13 +54,6 @@ app.use((req, res) => {
     res.end();
 });
 
-// io.use(ioCookie);
-
 server.listen(config.serverConfig.port, () => {
     console.log(`Server is running on port ${config.serverConfig.port}!`);
 });
-
-const model = require('./models/model-sequelize/model');
-model.User.sync();
-// model.User.sync()
-// model.User.sync()
---
+++ models/operator.js
@@ -0,0 +1,79 @@
+const socketio = require('socket.io');
+const ioCookie = require('socket.io-cookie');
+const model = require('./model-sequelize/model');
+
+const User = model.User
+    , Chatroom = model.Chatroom
+    , Membership = model.Membership
+    , Message = model.Message
+;
+
+class Operator {
+    constructor(server) {
+        this.io = socketio.listen(server);
+        this.io.sockets.on('connection', this.newSocket);
+    }
+}
+
+Operator.prototype.use = function (middleware) {
+    this.io.use(middleware);
+}
+
+Operator.prototype.newSocket = async function (socket) {
+    const userId = socket.handshake.session.userId;
+    let rooms = await this.getRoomIds(userId);
+    let user = await this.getUser(userId)
+    socket.join(rooms);
+
+    socket.on('join', (roomId) => {
+        socket.join(roomId);
+        socket.broadcast.to(data.roomId).emit('join', {
+            room_id: roomId,
+            username: user.username,
+            nickname: user.nickname,
+            profile: user.profile
+        });
+    });
+
+    socket.on('leave', (roomId) => {
+        socket.leave(roomId);
+        socket.broadcast.to(data.roomId).emit('leave', {
+            room_id: roomId,
+            username: user.username
+        });
+    });
+
+    socket.on('message', (data) => {
+        socket.broadcast.to(data.roomId).emit('message', {
+            room_id: data.roomId,
+            message: data.message
+        });
+        this.saveMessage(data.roomId, socket.handshake.session.userId, data.message);
+    })
+}
+
+
+Operator.prototype.getRoomIds = async function (userId) {
+    return await Membership.findAll({
+        where: {user_id: userId}
+    }).then(result => {
+        return result.map(r => r.room_id);
+    });
+}
+
+Operator.prototype.getUser = async function (userId) {
+    return await User.findOne({
+        where: {user_id: userId},
+        attributes: ['username', 'nickname', 'profile']
+    });
+}
+
+Operator.prototype.saveMessage = function (roomId, userId, text) {
+    Message.create({
+        room_id: roomId,
+        userId: userId,
+        text: text
+    });
+}
+
+module.exports = Operator;
