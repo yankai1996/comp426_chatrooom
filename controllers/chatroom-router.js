d01fc4bc9eb3ff19adfc8c420b606a2f7676d0a1
Kai Yan <u3514123@connect.hku.hk>
2020-11-19 19:35:08 -0500

    update respond data

--- controllers/chatroom-router.js
+++ controllers/chatroom-router.js
@@ -13,11 +13,16 @@ const checkSession = (req, res, next) => {
     }
 }
 
+const redirectedProfilePath = (subdir, profile) => {
+    return `image/${subdir}/${profile}`;
+}
+
 
 router.post('/chatroom/create', checkSession);
 router.post('/chatroom/create', async (req, res) => {
-    const result = await manager.create(req.session.userId, req.body);
-    if (result) res.send({room_id: result});
+    let room = await manager.create(req.session.userId, req.body);
+    room.profile = redirectedProfilePath('room', room.profile);
+    if (room) res.send(room);
     else res.status(400).end();
 });
 
@@ -25,44 +30,57 @@ router.post('/chatroom/create', async (req, res) => {
 router.post('/chatroom/leave', checkSession);
 router.post('/chatroom/leave', async (req, res) => {
     const success = await manager.leave(req.session.userId, req.body.room_id);
-    let status = success ? 200 : 400;
-    res.status(status).end();
+    res.status(success ? 200 : 400).end();
 });
 
 
 router.post('/chatroom/search', checkSession);
 router.post('/chatroom/search', async (req, res) => {
     let result = await manager.search(req.body.keyword);
+    result = result.map(r => {
+        r.profile = redirectedProfilePath('room', r.profile);
+        return r;
+    });
     res.send({chatrooms: result});
 });
 
 
 router.post('/chatroom/join', checkSession);
 router.post('/chatroom/join', async (req, res) => {
-    const result = await manager.join(req.session.userId, req.body.room_id);
+    let result = await manager.join(req.session.userId, req.body.room_id);
     if (result === false) res.status(500).end();
-    else res.send({users: result});
+    else {
+        result.room.profile = redirectedProfilePath('room', result.room.profile);
+        res.send(result);
+    }
 });
 
 
 router.post('/chatroom/get', checkSession);
 router.post('/chatroom/get', async (req, res) => {
-    const result = await manager.getRoomInfo(req.body.room_id);
+    let result = await manager.getRoomInfo(req.body.room_id);
+    result.users = result.users.map(u => {
+        u.profile = redirectedProfilePath('user', u.profile);
+        return u;
+    });
     res.send(result);
 });
 
 
 router.post('/homepage', checkSession);
 router.post('/homepage', async (req, res) => {
-    const rooms = await manager.getRoomsOfUser(req.session.userId);
-    const user = await manager.getUser(req.session.userId);
+    let rooms = await manager.getRoomsOfUser(req.session.userId);
+    let user = await manager.getUser(req.session.userId);
     if (rooms === false || !user) res.status(500).end();
     else res.send({
-        chatrooms: rooms,
+        chatrooms: rooms.map(r => {
+            r.profile = redirectedProfilePath('room', r.profile);
+            return r;
+        }),
         user: {
             username: user.username,
             nickname: user.nickname,
-            profile: user.profile
+            profile: redirectedProfilePath('user', user.profile)
         }
     });
 });
